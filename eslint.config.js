2cafa3a0dea3a3dec16e0506fa9b1ee29b95298c
Alex Carroll <alexandertcarroll@gmail.com>
2025-10-17 02:22:49 -0400

    fix: migrate to ESLint 9 flat config format
    - Replace .eslintrc.json with eslint.config.js
    - Remove deprecated .eslintignore (using ignores in config)
    - Simplify husky hooks to remove deprecated setup
    - Maintain all existing linting rules and prettier integration
    - Add eslint-disable comments for config file require statements
    
    This resolves the ESLint 9 compatibility issues and removes
    deprecation warnings from husky hooks.

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const tsParser = require('@typescript-eslint/parser');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const prettierPlugin = require('eslint-plugin-prettier');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      '.nx/**',
+      'coverage/**',
+      '**/*.spec.ts',
+      '**/*.test.ts',
+      '**/jest.config.ts',
+    ],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'prefer-arrow-callback': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 80,
+          tabWidth: 2,
+          semi: true,
+          bracketSpacing: true,
+          arrowParens: 'always',
+          endOfLine: 'lf',
+        },
+      ],
+    },
+  },
+];
